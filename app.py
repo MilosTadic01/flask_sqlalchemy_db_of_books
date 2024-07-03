ab31088f8e9a648e31104a675ae586054c772bda
Milos Tadic <milosgtadic@yahoo.com>
2024-07-04 00:45:03 +0200

    implement sort; implement search (but ignores sort values from the same page)

--- app.py
+++ app.py
@@ -1,27 +1,118 @@
 import os
+import requests
 from datetime import datetime
 from werkzeug.exceptions import BadRequest
 from flask import Flask, request, abort, jsonify, render_template
 from data_models import db, Author, Book
 
-AUTHOR_ENTRY_COMPONENTS = ["name", "birthdate", "date_of_death"]
-BOOK_ENTRY_COMPONENTS = ["title", "isbn", "publication_year", "author"]
-QUERY_AUTHORS = "SELECT name, author_id FROM authors;"
-
+GET_COVER_PREF = "https://covers.openlibrary.org/b/isbn/"
+GET_COVER_SUFF = "-M.jpg"
 
 app = Flask(__name__)
+
 # instead of 'engine = create_engine('sqlite:///data/restaurants.sqlite')':
 db_path = os.path.join(os.getcwd(), 'data', 'library.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 db.init_app(app)
 
 # Create tables, feel free to comment out after running once, though no need
-with app.app_context():
-    try:
-        db.create_all()
-        print("Tables created successfully.")
-    except Exception as e:
-        print(f"Error creating tables: {e}")
+# with app.app_context():
+#     try:
+#         db.create_all()
+#         print("Tables created successfully.")
+#     except Exception as e:
+#         print(f"Error creating tables: {e}")
+
+
+@app.route('/')
+def index():
+    """Render a page with database contents + book cover images. I don't
+    understand why we couldn't only fetch the cover image urls once and store
+    them in the DB, this is making the "rendering" much longer.
+
+    db.session.query allows column specs; returns [tuples]
+    Model.query (Book.query) forbids columns specs; returns [ORM objects] """
+    rows = db.session.query(Book.title, Book.author_id, Book.isbn).all()
+    sort_crit = request.args.get('sort')
+    sort_dir = request.args.get('dir')
+    if sort_crit:
+        rows = get_sorted_rows(sort_crit, sort_dir)
+    # Prepare a list of dicts to pass to Jinja2 in the html to render
+    books_list = []
+    for row in rows:
+        # automatically finds the existing db.session, no need to specify
+        author_entry = Author.query.filter(Author.author_id == row[1]).one()
+        author_name = "by <blank>"
+        if author_entry:
+            author_name = author_entry.name
+        cover_url = fetch_cover_url(row[2])
+        books_list.append(
+            {'title': row[0],
+             'author': author_name,
+             'cover_url': cover_url}
+        )
+    return render_template('home.html', books=books_list), 200
+
+
+def get_sorted_rows(sort_crit, sort_dir):
+    """Handle sorting args in the query string."""
+    if sort_crit == 'title':
+        if sort_dir == 'desc':
+            rows = db.session.query(Book.title, Book.author_id, Book.isbn) \
+                .order_by(Book.title.desc()).all()
+        else:
+            rows = db.session.query(Book.title, Book.author_id, Book.isbn) \
+                .order_by(Book.title).all()
+    elif sort_crit == 'author':
+        if sort_dir == 'desc':
+            rows = db.session.query(Book.title, Book.author_id, Book.isbn) \
+                .join(Author).order_by(Author.name.desc()).all()
+        else:
+            rows = db.session.query(Book.title, Book.author_id, Book.isbn) \
+                .join(Author).order_by(Author.name).all()
+    else:  # for 'year'
+        if sort_dir == 'desc':
+            rows = db.session.query(Book.title, Book.author_id, Book.isbn) \
+                .order_by(Book.year.desc()).all()
+        else:
+            rows = db.session.query(Book.title, Book.author_id, Book.isbn) \
+                .order_by(Book.year).all()
+    return rows
+
+
+@app.route('/search')
+def search():
+    """Returns a list of books where search query matches at least one of
+    ["title", "author", "publication_year"]. Year as string, so 199 works."""
+    search_term = request.args.get('search').lower()
+    rows = db.session.query(Book.title, Book.author_id, Book.isbn,
+                            Book.publication_year).all()
+    books_list = []
+    for row in rows:
+        author_entry = Author.query.filter(Author.author_id == row[1]).one()
+        author_name = "by <blank>"
+        if author_entry:
+            author_name = author_entry.name
+        if search_term not in row[0].lower() \
+                and search_term not in str(row[3]).lower() \
+                and search_term not in author_name.lower():
+            continue
+        cover_url = fetch_cover_url(row[2])
+        books_list.append(
+            {'title': row[0],
+             'author': author_name,
+             'cover_url': cover_url}
+        )
+    return render_template('home.html', books=books_list), 200
+
+
+def fetch_cover_url(isbn):
+    """Fetch an image from OpenLibrary API. If unsuccessful facilitate for the
+    displaying of alt= in the html."""
+    cover_url = requests.get(GET_COVER_PREF + isbn + GET_COVER_SUFF).url
+    if not cover_url:
+        cover_url = ""
+    return cover_url
 
 
 @app.route('/add_author', methods=['GET', 'POST'])
@@ -37,9 +128,9 @@ def add_author():
         name = request.form.get('name')
         birth = request.form.get('birthdate')
         death = request.form.get('date_of_death')
-        if not name or not birth:  # redundant with current html, but still
+        if not name or not birth:  # redundant with html 'required', but still
             abort(400)
-        new_author = Author()
+        new_author = Author()  # w/o attr 1st, so that I can check if 'death'
         new_author.name = name
         # Convert html date to Py datetime object, Flask-SQLAlchemy wants it
         new_author.birth_date = datetime.strptime(birth, '%Y-%m-%d')
@@ -57,6 +148,7 @@ def add_book():
     author input first."""
     if request.method == 'GET':
         authors_dict = {}
+
         # Previously with sqlalchemy:
         # results = connection.execute(text(QUERY))
         # rows = results.fetchall()
@@ -87,8 +179,7 @@ def add_book():
 
 @app.errorhandler(400)
 def error_bad_request(error):
-    """Handles bad sorting params for 'GET', missing info for 'POST'
-    and bad keys for 'PUT'."""
+    """Handles bad sorting params for 'GET' and missing info for 'POST'."""
     if request.method == 'GET':
         pass
     else:  # for 'POST' and 'PUT'
